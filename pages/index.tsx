2e0fa8e7ab756d0e67330d4b63d07a1f1505a590
fedeloterstein <federicoloterstein@gmail.com>
2022-11-03 18:53:58 -0300

    create shopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '../components/layouts/ShopLayout';
 
 export default function Home() {
   return (
-<h1>React Ecommerce</h1>
+<ShopLayout title={'React Ecommerce - Home'} pageDescription={'Encuentra los mejores productos de React Ecommerce'}>
+  <Typography variant='h1' component={'h1'}>Tienda</Typography>
+  <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
+</ShopLayout>
   )
 }

98a50509f95687cfc3a93f851436e9c7d6e50920
fedeloterstein <federicoloterstein@gmail.com>
2022-11-03 19:32:53 -0300

    create product card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
-import { Typography } from '@mui/material';
-import { ShopLayout } from '../components/layouts/ShopLayout';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts/ShopLayout'
+import { initialData } from '../database/products'
+
 
 export default function Home() {
   return (
-<ShopLayout title={'React Ecommerce - Home'} pageDescription={'Encuentra los mejores productos de React Ecommerce'}>
-  <Typography variant='h1' component={'h1'}>Tienda</Typography>
-  <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
-</ShopLayout>
+    <ShopLayout
+      title={'React Ecommerce - Home'}
+      pageDescription={'Encuentra los mejores productos de React Ecommerce'}
+    >
+      <Typography variant="h1" component={'h1'}>
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+      <Grid container spacing={4}>
+        {initialData.products.map(product => (
+        <Grid item xs={6} sm={4} key={product.slug}>
+          <Card>
+            <CardActionArea>
+              <CardMedia 
+              component={'img'}
+              image={`products/${product.images[0]}`}
+              alt={product.title}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        ))}
+      </Grid>
+    </ShopLayout>
   )
 }

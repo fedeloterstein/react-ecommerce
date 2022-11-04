6a653883cc2cde3ffc332092edbe5ab162e740e4
fedeloterstein <federicoloterstein@gmail.com>
2022-11-03 21:46:27 -0300

    create SizeSelector

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Chip, Grid, Typography } from '@mui/material'
 import React from 'react'
 import { ShopLayout } from '../../components/layouts'
 import { ProductSlideshow } from '../../components/products'
-import { ItemCounter } from '../../components/ui'
+import { ItemCounter, SizeSelector } from '../../components/ui'
 import { initialData } from '../../database/products'
 
 const product = initialData.products[0]
@@ -23,6 +23,7 @@ const slug = () => {
           <Box sx={{ my: 2 }}>
             <Typography variant="subtitle2">Cantidad</Typography>
             <ItemCounter />
+            <SizeSelector sizes={product.sizes}  />
           </Box>
           <Button color='secondary' className='circular-btn'>Agregar al Carrito</Button>
           <Chip  label='No hay disponibles' color='error' variant='outlined'/>

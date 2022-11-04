cacdfe4592a2843d6d39bf10aa6f8fc4999d1188
fedeloterstein <federicoloterstein@gmail.com>
2022-11-04 20:04:21 -0300

    add swr config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Typography } from '@mui/material'
 import { ShopLayout } from '../components/layouts/ShopLayout'
 import { ProductList } from '../components/products'
-import { initialData } from '../database/products';
+import { useProducts } from '../hooks'
 
 
 export default function Home() {
+
+const {products, isLoading} = useProducts('/products')
   return (
     <ShopLayout
       title={'React Ecommerce - Home'}
@@ -16,7 +18,12 @@ export default function Home() {
       <Typography variant="h2" sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
-      <ProductList products={initialData.products as any}/>
+      {
+        isLoading
+        ? <h1>Cargando...</h1>
+        : <ProductList products={products}/>
+      }
+  
     </ShopLayout>
   )
 }

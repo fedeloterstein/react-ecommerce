fc06d18b42cf59be1a6e5881f045179a6fd8b0cd
fedeloterstein <federicoloterstein@gmail.com>
2022-11-04 20:39:09 -0300

    create uiContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,22 @@ import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { lightTheme } from '../themes'
 import { SWRConfig } from 'swr'
-
+import { UiProvider } from '../context'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig 
-    value={{
-      fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-    }}
-  >
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </SWRConfig>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <UiProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
+    </SWRConfig>
   )
 }
